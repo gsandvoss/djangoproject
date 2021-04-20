e50c42076e0df4361b97a58f2af9d19dcc9a23f4
Glenn Sandvoss <gsandvoss@gmail.com>
2021-04-20 06:17:59 -0700

    added diety file table

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Files(models.Model):
+	diety_name = models.CharField(max_length=200)
+	diety_image = models.CharField(max_length=200)
+	file_name = models.CharField(max_length=200)
+	file_size = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.diety_name
