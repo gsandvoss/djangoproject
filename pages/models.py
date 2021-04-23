0c77f7891259857414ae5a2e0fe1ff7bd5ef02df
Glenn Sandvoss <gsandvoss@gmail.com>
2021-04-23 06:24:33 -0700

    added dynamic images to art files

--- pages/models.py
+++ pages/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+	
+
 
 class Files(models.Model):
 	diety_name = models.CharField(max_length=200)
--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 
 from django.urls import path
-from . import views 
+from . import views
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 
 urlpatterns = [
